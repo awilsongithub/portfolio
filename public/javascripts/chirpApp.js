268e8067fe4d79457960f54ebe80a577f5b7c3f4
Adam Wilson <adamwilson1234@gmail.com>
2016-01-23 19:27:14 -0600

    crud w authentication works! app done. tutorial done. can login, logout, register, post new chirp, see stream, angular hide/shows authentic based permissions and session based stuff works. next steps: customize functionality, styling, angular, etc...

--- public/javascripts/chirpApp.js
+++ public/javascripts/chirpApp.js
@@ -1,13 +1,15 @@
 // angular module construction named app, specifically chirpApp
-// [ ] holds dependencies
-// ngRoute is an additional script from the cdn
-var app = angular.module('chirpApp', ['ngRoute', 'ngResource']).run(function($rootScope){
-   // declare some rootScope variables.
-   // all ctrlrs have access to $rootScope
+// [ ] holds dependencies for the app ngRoute, ngResource
+// these are script linked in index.html to cdn
+var app = angular.module('chirpApp', ['ngRoute', 'ngResource']).run(function($rootScope, $http){
+   // DECLARE $ROOTSCOPE VARIABLES.
+   // ANGUULAR USES $ TO DENOTE PUBLIC/GLOBAL? OBJECTS USABLE ANYWHERE
+   // used here to carry auth status & current_user ...
+   // ... since app will behave differently based on these conditions
    $rootScope.authenticated = false; // by default
    $rootScope.current_user = '';
 
-   $rootScope.signout = function(){
+   $rootScope.logout= function(){
       // auth/signout calls logout() = mongoose method? that clears credentials?
       $http.get('auth/signout');
       $rootScope.authenticated = false;
@@ -15,8 +17,8 @@ var app = angular.module('chirpApp', ['ngRoute', 'ngResource']).run(function($ro
    };
 });
 
-// ANGULAR ROUTEPROVIDER
-// WIRES UP ROUTES, CTRLRS, TEMPLATES
+// ANGULAR ROUTEPROVIDER W/ NGROUTE
+// WHEN HIT URL X, USE TEMPLATE Y AND CTRLR Z
 // so we don't need call controllers frorm the view file
 app.config(function($routeProvider){
    $routeProvider
@@ -37,82 +39,94 @@ app.config(function($routeProvider){
    });
 });
 
+// RANDOM REMINDER: WHEN A PARAM PASSED > FUNCTION, IT'S PASSED TO
+// THE CODE BLOCK SCOPE OF THE FUNCTION. EX: doubleIt(x){ return x*2; }
+// http.get all posts from api, put in factory object
+// CALL IT POSTSERVICE, PAS IT HTTP AS IT WILL NEED IT.
+// WE'LL PASS THIS TO MAINCONTROLLER BY INJECTING IT IN PARAMS OF CTRLR
+// THIS FACTORY ABSTRACTS ALL THIS LOGIC AWAY SO WE DO IT ONCE AND CREATION
+// CALL IT IN DIF. PLACES IN APP. it's NAME IS POSTSERVICE
+
+// why commented out????
+// app.factory('postService', function($http){
+//    var baseUrl = 'api/posts'; // api
+//    var factory = {};  // object
+//    factory.getAll = function(){ // make http calls
+//       return $http.get(baseUrl); // use api defined
+//    };
+//    return factory; // has all posts data
+// });
+
+app.factory('postService', function($resource){
+   // using ngResource module takes endpoint api and creates many helper functions
+   // if included in a controller.
+   return $resource('/api/posts/:id');
+});
+
 // MAINCONTROLLER
 // is a function attaching a model onto $scope
 // items passed with $scope are in the scope of the view templates
-app.controller('mainController', function($scope, postService){
+app.controller('mainController', function($scope, $rootScope, postService){
    // attach variables onto our $scope (post objects, posts array)
-   $scope.posts = postService.query();
+   $scope.posts = postService.query(); // query is get req
    $scope.newPost = {created_by: '', text: '', created_at: ''};
 
-   postService.getAll().success(function(data){
-      $scope.posts = data;
-   });
+   // CALL POSTSERVICE (commented out since not in video at 3:48...)
+   // postService.getAll().success(function(data){
+   //    $scope.posts = data;
+   // });
 
    // POST CREATION FORM CALLS THIS FUNCTION
    // THIS NEW VERSION USES CURRENT_USER FROM THE LOGIN FUNCTION
-   // INSTEAD OF USERNAME VALUE FROM FORM PARAMETERS 
+   // INSTEAD OF USERNAME VALUE FROM FORM PARAMETERS
    // COMMENTED OUT OLD $SCOPE.POST FUNCTION PER MOD 5 NEW ONE (BELOW)
    $scope.post = function() {
       $scope.newPost.created_by = $rootScope.current_user;
       $scope.newPost.created_at = Date.now();
 
       postService.save($scope.newPost, function(){
-         $scope.posts = postService.quer();
-         $scope.newPost = {created_by: '', text: '', created_at: ''};
+         $scope.posts = postService.query(); // refresh post stream
+         $scope.newPost = {created_by: '', text: '', created_at: ''}; // clear new post
       });
-   };
+   }; // end post
 
-   // create post and push onto posts array
-   // called on form submit, adds timestamp, pushes onto post array
+}); // end maincontroller
+
+// create post and push onto posts array
+// called on form submit, adds timestamp, pushes onto post array
 //    $scope.post = function(){
 //       $scope.newPost.created_at = Date.now();
 //       $scope.posts.push($scope.newPost);
 //       // resest newPost
 //       $scope.newPost = {created_by: '', text: '', created_at: ''};
 //   };
-// });
 
-
-// RANDOM REMINDER: WHEN A PARAM PASSED > FUNCTION, IT'S PASSED TO
-// THE CODE BLOCK SCOPE OF THE FUNCTION. EX: doubleIt(x){ return x*2; }
-// http.get all posts from api, put in factory object
-app.factory('postService', function($http){
-   var baseUrl = 'api/posts';
-   var factory = {};
-   factory.getAll = function(){
-      return.$http.get(baseUrl);
-   };
-   return factory;
-});
-
-app.factory('postService', function($resource){
-   return $resource('/api/posts/:id');
-});
 
 // AUTHENTICATION CONTROLLER
+// $location is used for redirects
 app.controller('authController', function($scope, $http, $rootScope, $location){
    // create or identify user with key values from form
    $scope.user = {username: '', password: ''};
    $scope.error_message = '';
 
    $scope.login = function(){
+      // pass user to auth/login
       // post to this api, get user object for current user = data
+      // ajax call syntax: $http.httpmethod('/url').success(successCallback)
       $http.post('/auth/login', $scope.user).success(function(data){
          if(data.state == 'success'){
             $rootScope.authenticated = true;
             $rootScope.current_user = data.user.username;
-            $location.path('/');
+            $location.path('/'); // redirect
          }
          else{
             $scope.error_message = data.message;
          }
-      });
-   };
+      }); // end success
+   }; // end login
 
+   // almost same as login function
    $scope.register = function(){
-      // http.post req w/user set authentication to true
-      // and get user from data, then redirect to rootScope
       $http.post('/auth/signup', $scope.user).success(function(data){
          if(data.state == 'success'){
             $rootScope.authenticated = 'true';
@@ -122,6 +136,6 @@ app.controller('authController', function($scope, $http, $rootScope, $location){
          else{
             $scope.error_message = data.message;
          }
-      });
-   };
-});
+      }); // end success
+   }; // end register
+}); // end authcontroller
